db0e65605c039079422bbc3719b4fa7ecd881c45
Roberto Rojas <developer@robertorojas.com.mx>
2022-09-19 23:55:03 -0500

    Adding control variables and environment set process

--- gorminal.go
+++ gorminal.go
@@ -2,15 +2,13 @@ package gorminal
 
 import (
 	"github.com/RobertoRojas/golor"
+	"github.com/nathan-fiscaletti/consolesize-go"
 )
+
+var OverrideEnv bool
 
 var colorMode golor.GolorMode
 var colorEnv bool
-
-var soundMode golor.GolorSound
-var soundEnv bool
-
-var OverrideEnv bool
 
 func SetColorMode(m golor.GolorMode) {
 	if colorEnv && !OverrideEnv {
@@ -19,9 +17,99 @@ func SetColorMode(m golor.GolorMode) {
 	colorMode = m
 }
 
+var soundMode golor.GolorSound
+var soundEnv bool
+
 func SetSoundMode(m golor.GolorSound) {
 	if soundEnv && !OverrideEnv {
 		return
 	}
 	soundMode = m
+}
+
+var dynamicSize bool
+var dynamicSizeEnv bool
+
+func SetDynamicSize(d bool) {
+	if dynamicSizeEnv && !OverrideEnv {
+		return
+	}
+	dynamicSizeEnv = d
+}
+
+var defTermSize int
+var defTermSizeEnv bool
+
+func SetDefTermSize(s int) {
+	if s < 1 {
+		return
+	}
+	defTermSize = s
+}
+
+func getTermSize() (int, int) {
+	if !dynamicSize {
+		return int(defTermSize), 0
+	}
+	return consolesize.GetConsoleSize()
+}
+
+var messageLevel int
+var messageLevelEnv bool
+
+func SetMessageLevel(l int) {
+	if messageLevelEnv && !OverrideEnv {
+		return
+	}
+	messageLevel = l
+}
+
+var errorLevel int
+var errorLevelEnv bool
+
+func SetErrorLevel(l int) {
+	if errorLevelEnv && !OverrideEnv {
+		return
+	}
+	errorLevel = l
+}
+
+var debugLevel int
+var debugLevelEnv bool
+
+func SetDebugLevel(l int) {
+	if debugLevelEnv && !OverrideEnv {
+		return
+	}
+	debugLevel = l
+}
+
+var warningLevel int
+var warningLevelEnv bool
+
+func SetWarningLevel(l int) {
+	if warningLevelEnv && !OverrideEnv {
+		return
+	}
+	warningLevel = l
+}
+
+var verboseLevel int
+var verboseLevelEnv bool
+
+func SetVerboseLevel(l int) {
+	if verboseLevelEnv && !OverrideEnv {
+		return
+	}
+	verboseLevel = l
+}
+
+var outputLevel int
+var outputLevelEnv bool
+
+func SetOutputLevel(l int) {
+	if outputLevelEnv && !OverrideEnv {
+		return
+	}
+	outputLevel = l
 }
--- init.go
+++ init.go
@@ -2,10 +2,28 @@ package gorminal
 
 import (
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/RobertoRojas/golor"
 )
+
+func getIntFromEnv(eVar string, def int) (int, bool) {
+	if v := os.Getenv(eVar); len(v) > 0 {
+		u, err := strconv.ParseInt(v, 10, 32)
+		if err == nil {
+			return int(u), true
+		}
+	}
+	return def, false
+}
+
+func getBoolFromEnv(eVar string, def bool) (bool, bool) {
+	if v := os.Getenv(eVar); len(v) > 0 {
+		return strings.ToLower(v) == "true", true
+	}
+	return def, false
+}
 
 func init() {
 	if v := os.Getenv("GORMIAL_COLOR_MODE"); len(v) > 0 {
@@ -36,5 +54,16 @@ func init() {
 		soundMode = golor.Mute
 		soundEnv = false
 	}
+	dynamicSize, dynamicSizeEnv = getBoolFromEnv("GORMIAL_DYNAMIC_SIZE", true)
+	defTermSize, defTermSizeEnv = getIntFromEnv("GORMIAL_DEFAULT_TERMINAL_SIZE", 58)
+	if defTermSize < 1 {
+		defTermSize = 58
+	}
+	messageLevel, messageLevelEnv = getIntFromEnv("GORMIAL_", 1)
+	errorLevel, errorLevelEnv = getIntFromEnv("GORMIAL_MESSAGE_LEVEL", 1)
+	debugLevel, debugLevelEnv = getIntFromEnv("GORMIAL_ERROR_LEVEL", 0)
+	warningLevel, warningLevelEnv = getIntFromEnv("GORMIAL_WARNING_LEVEL", 1)
+	verboseLevel, verboseLevelEnv = getIntFromEnv("GORMIAL_VERBOSE_LEVEL", 0)
+	outputLevel, outputLevelEnv = getIntFromEnv("GORMIAL_OUTPUT_LEVEL", 1)
 	OverrideEnv = false
 }
