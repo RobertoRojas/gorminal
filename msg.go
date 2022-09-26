60045015b810a5feabd370b093205c14061fd4bc
Roberto Rojas <developer@robertorojas.com.mx>
2022-09-25 21:03:09 -0500

    Adding output functions and STD variables

--- msg.go
+++ msg.go
@@ -1,68 +1,69 @@
 package gorminal
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 
 	"github.com/RobertoRojas/golor"
 )
 
-func Show(m string, c bool, f *os.File) {
+func Show(m string, c bool, f os.File) {
 	if !c {
 		return
 	}
-	fmt.Fprint(f, m)
+	fmt.Fprint(&f, m)
 }
 
-func Showln(m string, c bool, f *os.File) {
+func Showln(m string, c bool, f os.File) {
 	if !c {
 		return
 	}
-	fmt.Fprintln(f, m)
+	fmt.Fprintln(&f, m)
 }
 
 func ShowMsg(m string, l int) {
-	Show(m, l <= messageLevel, os.Stdout)
+	Show(m, l <= messageLevel && !quiet, messageSTD)
 }
 
 func ShowMsgCond(m string, l int, c bool) {
-	Show(m, l <= messageLevel && c, os.Stdout)
+	Show(m, l <= messageLevel && c && !quiet, messageSTD)
 }
 
 func ShowMsgLn(m string, l int) {
-	Showln(m, l <= messageLevel, os.Stdout)
+	Showln(m, l <= messageLevel && !quiet, messageSTD)
 }
 
 func ShowMsgCondln(m string, l int, c bool) {
-	Showln(m, l <= messageLevel && c, os.Stdout)
+	Showln(m, l <= messageLevel && c && !quiet, messageSTD)
 }
 
 func ShowErrMsg(m string, l int) {
 	g := golor.New()
 	g.Bell().Red().ColorBit256(196).
 		ColorBitRGB(255, 0, 0).Add(m).Reset()
-	Show(g.String(), l <= errorLevel, os.Stderr)
+	Show(g.String(), l <= errorLevel && !quiet, errorSTD)
 }
 
 func ShowErrMsgCond(m string, l int, c bool) {
 	g := golor.New()
 	g.Bell().Red().ColorBit256(196).
 		ColorBitRGB(255, 0, 0).Add(m).Reset()
-	Show(g.String(), l <= errorLevel && c, os.Stderr)
+	Show(g.String(), l <= errorLevel && c && !quiet, errorSTD)
 }
 
 func ShowErrMsgln(m string, l int) {
 	g := golor.New()
 	g.Bell().Red().ColorBit256(196).
 		ColorBitRGB(255, 0, 0).Add(m).Reset()
-	Showln(g.String(), l <= errorLevel, os.Stderr)
+	Showln(g.String(), l <= errorLevel && !quiet, errorSTD)
 }
 
 func ShowErrMsgCondln(m string, l int, c bool) {
 	g := golor.New()
 	g.Bell().Red().ColorBit256(196).
 		ColorBitRGB(255, 0, 0).Add(m).Reset()
-	Showln(g.String(), l <= errorLevel && c, os.Stderr)
+	Showln(g.String(), l <= errorLevel && c && !quiet, errorSTD)
 }
 
 func ShowDbgMsg(m string, l int) {
@@ -70,7 +71,7 @@ func ShowDbgMsg(m string, l int) {
 	g.Magenta().ColorBit256(63).
 		ColorBitRGB(153, 51, 255).Bold().
 		Add("[DEBUG] ").BoldReset().Add(m).Reset()
-	Show(g.String(), l <= debugLevel, os.Stdout)
+	Show(g.String(), l <= debugLevel && !quiet, debugSTD)
 }
 
 func ShowDbgMsgCond(m string, l int, c bool) {
@@ -78,7 +79,7 @@ func ShowDbgMsgCond(m string, l int, c bool) {
 	g.Magenta().ColorBit256(63).
 		ColorBitRGB(153, 51, 255).Bold().
 		Add("[DEBUG] ").BoldReset().Add(m).Reset()
-	Show(g.String(), l <= debugLevel && c, os.Stdout)
+	Show(g.String(), l <= debugLevel && c && !quiet, debugSTD)
 }
 
 func ShowDbgMsgln(m string, l int) {
@@ -86,7 +87,7 @@ func ShowDbgMsgln(m string, l int) {
 	g.Magenta().ColorBit256(63).
 		ColorBitRGB(153, 51, 255).Bold().
 		Add("[DEBUG] ").BoldReset().Add(m).Reset()
-	Showln(g.String(), l <= debugLevel, os.Stdout)
+	Showln(g.String(), l <= debugLevel && !quiet, debugSTD)
 }
 
 func ShowDbgMsgCondln(m string, l int, c bool) {
@@ -94,7 +95,7 @@ func ShowDbgMsgCondln(m string, l int, c bool) {
 	g.Magenta().ColorBit256(63).
 		ColorBitRGB(153, 51, 255).Bold().
 		Add("[DEBUG] ").BoldReset().Add(m).Reset()
-	Showln(g.String(), l <= debugLevel && c, os.Stdout)
+	Showln(g.String(), l <= debugLevel && c && !quiet, debugSTD)
 }
 
 func ShowWrnMsg(m string, l int) {
@@ -102,7 +103,7 @@ func ShowWrnMsg(m string, l int) {
 	g.Yellow().ColorBit256(190).
 		ColorBitRGB(255, 255, 0).Bold().
 		Add("[WARNING] ").BoldReset().Add(m).Reset()
-	Show(g.String(), l <= warningLevel, os.Stdout)
+	Show(g.String(), l <= warningLevel && !quiet, warningSTD)
 }
 
 func ShowWrnMsgCond(m string, l int, c bool) {
@@ -110,7 +111,7 @@ func ShowWrnMsgCond(m string, l int, c bool) {
 	g.Yellow().ColorBit256(190).
 		ColorBitRGB(255, 255, 0).Bold().
 		Add("[WARNING] ").BoldReset().Add(m).Reset()
-	Show(g.String(), l <= warningLevel && c, os.Stdout)
+	Show(g.String(), l <= warningLevel && c && !quiet, warningSTD)
 }
 
 func ShowWrnMsgln(m string, l int) {
@@ -118,7 +119,7 @@ func ShowWrnMsgln(m string, l int) {
 	g.Yellow().ColorBit256(190).
 		ColorBitRGB(255, 255, 0).Bold().
 		Add("[WARNING] ").BoldReset().Add(m).Reset()
-	Showln(g.String(), l <= warningLevel, os.Stdout)
+	Showln(g.String(), l <= warningLevel && !quiet, warningSTD)
 }
 
 func ShowWrnMsgCondln(m string, l int, c bool) {
@@ -126,7 +127,7 @@ func ShowWrnMsgCondln(m string, l int, c bool) {
 	g.Yellow().ColorBit256(190).
 		ColorBitRGB(255, 255, 0).Bold().
 		Add("[WARNING] ").BoldReset().Add(m).Reset()
-	Showln(g.String(), l <= warningLevel && c, os.Stdout)
+	Showln(g.String(), l <= warningLevel && c && !quiet, warningSTD)
 }
 
 func ShowVerbMsg(m string, l int) {
@@ -134,7 +135,7 @@ func ShowVerbMsg(m string, l int) {
 	g.Cyan().ColorBit256(44).
 		ColorBitRGB(0, 255, 255).Bold().
 		Add("[VERBOSE] ").BoldReset().Add(m).Reset()
-	Show(g.String(), l <= verboseLevel, os.Stdout)
+	Show(g.String(), l <= verboseLevel && !quiet, verboseSTD)
 }
 
 func ShowVerbMsgCond(m string, l int, c bool) {
@@ -142,7 +143,7 @@ func ShowVerbMsgCond(m string, l int, c bool) {
 	g.Cyan().ColorBit256(44).
 		ColorBitRGB(0, 255, 255).Bold().
 		Add("[VERBOSE] ").BoldReset().Add(m).Reset()
-	Show(g.String(), l <= verboseLevel && c, os.Stdout)
+	Show(g.String(), l <= verboseLevel && c && !quiet, verboseSTD)
 }
 
 func ShowVerbMsgln(m string, l int) {
@@ -150,7 +151,7 @@ func ShowVerbMsgln(m string, l int) {
 	g.Cyan().ColorBit256(44).
 		ColorBitRGB(0, 255, 255).Bold().
 		Add("[VERBOSE] ").BoldReset().Add(m).Reset()
-	Showln(g.String(), l <= verboseLevel, os.Stdout)
+	Showln(g.String(), l <= verboseLevel && !quiet, verboseSTD)
 }
 
 func ShowVerbMsgCondln(m string, l int, c bool) {
@@ -158,5 +159,27 @@ func ShowVerbMsgCondln(m string, l int, c bool) {
 	g.Cyan().ColorBit256(44).
 		ColorBitRGB(0, 255, 255).Bold().
 		Add("[VERBOSE] ").BoldReset().Add(m).Reset()
-	Showln(g.String(), l <= verboseLevel && c, os.Stdout)
+	Showln(g.String(), l <= verboseLevel && c && !quiet, verboseSTD)
+}
+
+func ShowOutput(o string, l int) {
+	Showln(o, l == outputLevel && output, outputSTD)
+}
+
+func ShowOutputStruc(o interface{}, l int) {
+	j, err := json.Marshal(o)
+	if err != nil {
+		ShowErrMsg(err.Error(), 0)
+		return
+	}
+	ShowOutput(string(j), l)
+}
+
+func ShowOutputStrucIndent(o interface{}, prefix string, indent string, l int) {
+	j, err := json.MarshalIndent(o, prefix, indent)
+	if err != nil {
+		ShowErrMsg(err.Error(), 0)
+		return
+	}
+	ShowOutput(string(j), l)
 }
